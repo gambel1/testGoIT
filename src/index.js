1e93228322082f4e0c65e3e8797b2243b1fbb46b
Алексей <electrifyx@gmail.com>
2023-06-04 12:33:37 +0300

    edit basename in BrowserRouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/testGoIT">
+    <BrowserRouter basename="/testGoIT/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
