81ce709de5e704ae351dee17514a2ad3a1317019
Алексей <electrifyx@gmail.com>
2023-06-05 16:18:16 +0300

    edit Filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
-// import { Menu, MenuItem, Tooltip, Fade } from "@mui/material";
-import { statusFilters } from "../../refs/constants";
-// import { FilterAlt } from "@mui/icons-material/";
-// import {
-//   buttonFilterStyle,
-//   textStyle,
-//   iconFilterStyle,
-//   menuItemsStyle,
-// } from "./filterStyles";
+import {
+  Button,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography,
+  Fade,
+} from "@mui/material";
 
-export default function Filter({ value = "Show all", onChange }) {
+import { statusFilters } from "../../refs/constants";
+
+export default function Filter  ({ value = "Show all", onChange })  {
   const [anchorEl, setAnchorEl] = useState("");
   const [selectedItem, setSelectedItem] = useState(value);
 
@@ -25,38 +26,44 @@ export default function Filter({ value = "Show all", onChange }) {
 
   return (
     <>
-      <div title="Filter" placement="left" arrow>
-        <button
+      <Tooltip title="Filter" placement="left" arrow>
+        <Button
           id="fade-button"
           aria-controls={open ? "fade-menu" : undefined}
           aria-haspopup="true"
           aria-expanded={open ? "true" : undefined}
           onClick={handleClick}
+      
         >
-          <span>{value}</span>
-        </button>
-      </div>
+          <Typography >{value}</Typography>
+          
+        </Button>
+      </Tooltip>
 
-      <ul
-        
+      <Menu
+        id="fade-menu"
+        MenuListProps={{ "aria-labelledby": "fade-button" }}
+        anchorEl={anchorEl}
+        open={open}
         onClose={handleClose}
-        
+        TransitionComponent={Fade}
       >
         {statusFilters.map((item) => {
           return (
-            <li
+            <MenuItem
               key={item}
               disabled={item === selectedItem}
               selected={item === selectedItem}
               onClick={() => {
                 onChange(item, setAnchorEl, setSelectedItem);
               }}
+              
             >
               {item}
-            </li>
+            </MenuItem>
           );
         })}
-      </ul>
+      </Menu>
     </>
   );
-}
+};
