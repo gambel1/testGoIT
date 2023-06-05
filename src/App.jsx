737b9bbc90d92671eea1f1282d508d3643c47b76
Алексей <electrifyx@gmail.com>
2023-06-05 17:33:06 +0300

    delete Not Found 404 with App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
 import Tweets from "./pages/Tweets/Tweets";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="tweets" element={<Tweets />} />
+        <Route path="/tweets" element={<Tweets />} />
 
-        <Route path="*" element={<div>Not Found 404</div>} />
+        <Route path="*" element={<Navigate to={"/"} />} />
       </Route>
     </Routes>
   );
